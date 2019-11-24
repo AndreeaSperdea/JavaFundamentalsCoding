638be879f8fbf437d9a4ba6e6c44a66e70af5942
Andreea Sperdea <andreea.sperdea@gmail.com>
2019-11-24 10:54:06 +0200

    punctul 2

--- src/andreea/company/ex12.java
+++ src/andreea/company/ex12.java
@@ -4,25 +4,45 @@ import java.util.Scanner;
 
 public class ex12 {
     public static void main(String[] args) {
-        suma();
-
-    }
-    public static void suma()
-    {
-
         Scanner in = new Scanner(System.in);
 
+        suma(in);
+        average(in);
+
+    }
+    public static void suma(Scanner in)
+    {
         System.out.println("Dati lungimea array-ului: ");
         int dim = Integer.parseInt(in.nextLine());
 
-        int [] array = new int[dim];
-int sum = 0;
-        for (int i = 0; i < dim ; i++) {
-            System.out.println("array["+(i+1)+"]=" );
-            array[i] = Integer.parseInt(in.nextLine());
-            sum = sum+array[i];
-        }
-        System.out.println("Suma este: "+sum);
+        int [] array = readArray(in,dim);
+
+        System.out.println("Suma este: "+sumArray(array));
+    }
+    public static void average(Scanner in){
+        System.out.println("Dati dimensiune: ");
+        int dim = in.nextInt();
+        int[] array = readArray(in, dim);
+        double average = (double) sumArray(array)/dim;
+        System.out.println("Media elementelor este: "+ average);
+
     }
 
+public static int[] readArray(Scanner in, int dimensiune) {
+
+    int[] array = new int[dimensiune];
+
+    for (int i = 0; i < array.length; i++) {
+        System.out.println("array[" + (i + 1) + "]=");
+        array[i] = in.nextInt();
+    }
+    return array;
+}
+public static int sumArray(int[] array){
+    int sum = 0;
+    for (int i = 0; i < array.length; i++) {
+        sum = sum+array[i];
+    }
+       return sum;
+}
 }
