b2cbe09ef8cfa6eedace7c6470f3103b1dabd110
Andreea Sperdea <andreea.sperdea@gmail.com>
2019-11-24 11:52:28 +0200

    Punct3

--- src/andreea/company/ex12.java
+++ src/andreea/company/ex12.java
@@ -8,41 +8,83 @@ public class ex12 {
 
         suma(in);
         average(in);
+        punctul3(in);
 
     }
-    public static void suma(Scanner in)
-    {
+
+    public static void suma(Scanner in) {
         System.out.println("Dati lungimea array-ului: ");
         int dim = Integer.parseInt(in.nextLine());
 
-        int [] array = readArray(in,dim);
+        int[] array = readArray(in, dim);
 
-        System.out.println("Suma este: "+sumArray(array));
+        System.out.println("Suma este: " + sumArray(array));
     }
-    public static void average(Scanner in){
+
+    public static void average(Scanner in) {
         System.out.println("Dati dimensiune: ");
         int dim = in.nextInt();
         int[] array = readArray(in, dim);
-        double average = (double) sumArray(array)/dim;
-        System.out.println("Media elementelor este: "+ average);
+        double average = (double) sumArray(array) / dim;
+        System.out.println("Media elementelor este: " + average);
 
     }
 
-public static int[] readArray(Scanner in, int dimensiune) {
+    public static void punctul3(Scanner in) {
+        System.out.println("Dati dimensiune: ");
+        int dim = in.nextInt();
+        int[] array = readArray(in, dim);
+        System.out.println("Dimensiunea initiala este: " + array.length);
+        printArray(array);
+        System.out.println("Dati indexul elementului:");
+        int index = in.nextInt();
+        array = extractPosition(array, index);
+        System.out.println("Dimensiunea finala: " + array.length);
+        printArray(array);
 
-    int[] array = new int[dimensiune];
+    }
 
-    for (int i = 0; i < array.length; i++) {
-        System.out.println("array[" + (i + 1) + "]=");
-        array[i] = in.nextInt();
+    public static int[] readArray(Scanner in, int dimensiune) {
+
+        int[] array = new int[dimensiune];
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("array[" + (i + 1) + "]=");
+            array[i] = in.nextInt();
+        }
+        return array;
     }
-    return array;
-}
-public static int sumArray(int[] array){
-    int sum = 0;
-    for (int i = 0; i < array.length; i++) {
-        sum = sum+array[i];
+
+    public static int sumArray(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum = sum + array[i];
+        }
+        return sum;
     }
-       return sum;
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+    }
+
+    public static int[] extractPosition(int[] array, int index) {
+        int[] newArray = new int[array.length - 1];
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+
+            if (i == index) {
+                continue;
+            } else {
+                newArray[count] = array[i];
+                count++;
+            }
+        }
+        return newArray;
+    }
 }
-}
+
+
+
